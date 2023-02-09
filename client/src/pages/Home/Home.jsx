2f4509eb06002986b801d3fecd4c0f9801b22c4c
Sandy Pestaña <sandy.milena7@gmail.com>
2023-02-08 20:46:55 -0500

    avanzando en el front 3

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,19 @@
+// Importando react y redux
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// Importamos la accion
+// Importando la accion
 import { pokemonsAction } from "../../actions/pokemonsAction";
 import { pokemonByTypeAction } from "../../actions/pokemonByTypeAction";
+// Importando imagenes
 import loading from "../../images/loading.gif";
-import style from "./Home.module.css";
+// Importando componentes
 import Navbar from "../../components/Navbar/Navbar";
 import Card from "../../components/Card/Card";
+
+// Importando los filtrados de utils
+import { tipos } from "../../utils/filtros";
+// Importando estilos
+import style from "./Home.module.css";
 
 // {}
 
@@ -18,10 +25,12 @@ const Home = () => {
   const dispatch = useDispatch();
 
   // Selector
-  const pokemons = useSelector((state) => state.pokemons.data);
+  let pokemons = useSelector((state) => state.pokemons.data);
   const isLoading = useSelector((state) => state.pokemons.isLoading);
   const pokemonsTypes = useSelector((state) => state.pokemonsByTypes.data);
 
+  if (pokemonType) pokemons = tipos(pokemonType, pokemons);
+  console.log(pokemonType, "hola");
   useEffect(() => {
     if (pokemons.length === 0 || pokemonsTypes.length === 0) {
       dispatch(pokemonsAction());
