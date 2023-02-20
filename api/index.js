0e6635b0a6422c528802ab5895060ccad9939abb
Sandy Pestaña <sandy.milena7@gmail.com>
2023-02-20 10:50:01 -0500

    refactorizando index.js back

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ require("dotenv").config();
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, async () => {
-    await getAllTypes();
+    //await getAllTypes();
     console.log("%s listening at", process.env.PORT); // eslint-disable-line no-console
   });
 });
